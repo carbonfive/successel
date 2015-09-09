68ee37ec4de222540927ab70aad893d8de34b020
Erik Ingenito <erik@carbonfive.com>
2015-09-09 16:11:05 -0700

    Working polling of server

--- device/network_data_transfer.js
+++ device/network_data_transfer.js
@@ -19,17 +19,20 @@ function getClimateData() {
     climate.readHumidity(function (err, humid) {
       console.log('Degrees:', temp.toFixed(4) + 'C', 'Humidity:', humid.toFixed(4) + '%RH');
       postData({
-        "time": time,
-        "temp": temp.toFixed(4),
-        "humid": humid.toFixed(4)
+        "time": Math.floor(Date.now() / 1000),
+        "climate": {
+          "temperature": temp.toFixed(4),
+          "humidity": humid.toFixed(4)
+        }
       })
     });
   });
 }
 
 function postData(data) {
-  console.log("put to %s", config.controllerUrl)
-  var t = (new Date()).getMilliseconds()
+  console.log("%j", data);
+  console.log("put to %s", config.controllerUrl);
+  var t = (new Date()).getMilliseconds();
   needle.put( config.controllerUrl, data, function(error, resp) {
     var success = (!error && resp.statusCode == 200);
 
@@ -38,35 +41,38 @@ function postData(data) {
     } else {
       console.log("FAIL", error)
     }
+
+    setTimeout( getClimateData, 100)
   });
 }
 
 console.log("Binding to the ambient ready signal");
-
+var timeouts = 0+config.wifi.maxTimeouts;
 climate.on('ready', function (err) {
-  setInterval( getClimateData, 1000)
-  console.log("CLIMATE ERROR: " + err)
+  getClimateData()
+  console.log("Climate Module Ready")
 });
-
-wifi.on("connect", function(data) {
-  console.log('wifi> on:connect', data);
-});
-wifi.on("disconnect", function(data) {
-  console.log('wifi> on:disconnect', data);
-});
-wifi.on("timeout", function(data) {
-  console.log('wifi> on:timeout', data);
-  if (timeouts-- > 0) {
-    console.log("simple reconnect...");
-    wifiManager.connect()
-  } else {
-    wifiManager.powerCycle()
-  }
-});
-wifi.on("error", function(data) {
-  console.log('wifi> on:error', data);
-});
-
-console.log("Running...")
-console.log("%j", wifiManager)
-wifiManager.connect()
+////
+//wifi.on("connect", function(data) {
+//  console.log('wifi> on:connect', data);
+//});
+//wifi.on("disconnect", function(data) {
+//  console.log('wifi> on:disconnect', data);
+//});
+//wifi.on("timeout", function(data) {
+//  console.log('wifi> on:timeout', data);
+//  if (timeouts-- > 0) {
+//    console.log("timeout...");
+////    wifiManager.connect()
+//  } else {
+//    timeouts = 0+config.wifi.maxTimeouts;
+//    wifiManager.powerCycle()
+//  }
+//});
+//wifi.on("error", function(data) {
+//  console.log('wifi> on:error', data);
+//});
+//
+//console.log("Running...")
+//console.log("%j", wifiManager)
+//wifiManager.connect()
